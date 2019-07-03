618b7eb41e482b42a80162db3cc25a2490d3b0c4
Brandon Wiedemeier <brandon.wiedemeier@gmail.com>
2019-07-03 14:37:22 -0500

    final practice

---
+++ variables.js
@@ -0,0 +1,12 @@
+console.log( 'js' );
+
+// declare and initialize a variable
+let testVar = 9;
+console.log( testVar );
+console.log( 'the value of xxxx is', testVar);
+testVar = 1234
+console.log( 'the value of xxxx is', testVar);
+testVar = 10
+console.log( 'the value of xxxx is', testVar);
+testVar--;
+testVar-=5;
